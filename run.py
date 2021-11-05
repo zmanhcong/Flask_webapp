06962b5acaa310b615b98f7d59e3f2d442a466ed
zmanhcong <zmanhcong@gmail.com>
2021-11-05 10:34:53 +0900

    done

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from market import app
 
 #checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True) #auto Synchronous.
+    app.run(debug=True) #auto Synchronous.vvvv
 
